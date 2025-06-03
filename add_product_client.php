1ab881dcc5c99e1e897e732ff7b9d4dca954990e
datAfonso570 <1240570@isep.ipp.pt>
2025-06-03 12:38:05 +0100

    Att
    Att

---
+++ add_product_client.php
@@ -0,0 +1,25 @@
+<?php
+$product_name = $_POST['product_name'];
+$supplier_name = $_POST['supplier_name'];
+$product_price = $_POST['product_price'];
+$product_category = $_POST['product_category'];
+$product_notes = $_POST['product_notes'];
+$product_data = $_POST['product_date'];
+$client_name = $_POST['client_name'];
+$client_email = $_POST['client_email'];
+$client_nif = $_POST['client_nif'];
+$payment_method = $_POST['payment_method'];
+$client_phone = $_POST['client_phone'];
+$client_data = $_POST['client_date'];
+
+
+
+
+
+
+
+
+
+
+
+?>
