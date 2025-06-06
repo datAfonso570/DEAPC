b4ffd0c947e9efee915198c4e6789d4c3871a383
AP <174379297+AP-III@users.noreply.github.com>
2025-06-06 03:41:31 +0100

    figured out how to send and receive info from server to client in a way that's easy to access

--- query.php
+++ query.php
@@ -1,17 +1,24 @@
-<html>
-<body>
+
    <?php
-   $url="localhost/deapc/".$_POST["url"];
+   /*session_start();
+    if(!isset($_SESSION['logedin'])){
+        header('Location:http://localhost/deapc/index.html');
+        session_destroy();
+    }*/
+   //$url="localhost/deapc/".$_POST["url"];
    //include($_POST["url"]);
     libxml_use_internal_errors(true);
-    $htmlDoc = new DOMDocument();
+   /*$htmlDoc = new DOMDocument();
     $htmlDoc->loadHTMLFile($_POST["url"]);
     //echo $htmlDoc->saveHTML();
     $id='orderID';
     $htmlDoc->getElementbyID($id)->nodeValue="changed2 again";
-    echo $htmlDoc->saveHTML();
-
+    echo $htmlDoc->saveHTML();*/
+    $s = [];
+    $s['id']='asdasd';
+    $s['id3']=['asdasd5','werwer','werwr'];  
+    $s['id5']='asdasd6';      
+    $j=json_encode($s);
+    echo $j;
     ?> 
-</body>
 
-</html>
